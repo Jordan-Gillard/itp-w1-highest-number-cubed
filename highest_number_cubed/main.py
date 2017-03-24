19db9f1b37978d62737aa7c042a2110edf5eea19
Jordan Gillard <jordan-gillard@outlook.com>
2017-03-24 00:48:42 +0000

    got the program to work

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,31 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    numbers_below_limit = []
+    for i in range(limit):
+        val = i ** 3
+        if val < limit:
+            numbers_below_limit.append(i)
+        else:
+            return numbers_below_limit[-1]
+            
+
+# import unittest
+
+# #from highest_number_cubed import highest_number_cubed
+
+
+# class TestHighestNumberCubed(unittest.TestCase):
+
+#     def test_three(self):
+#         self.assertEqual(highest_number_cubed(30), 3)
+
+#     def test_two(self):
+#         self.assertEqual(highest_number_cubed(12), 2)
+
+#     def test_one(self):
+#         self.assertEqual(highest_number_cubed(3), 1)
+
+#     def test_big(self):
+#         self.assertEqual(highest_number_cubed(12000), 22)
+# unittest.main()
